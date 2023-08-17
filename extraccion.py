c927dd092b09ff66388d9fe9c98f269110f98f37
pedroest13 <120059042+pedroest13@users.noreply.github.com>
2023-08-17 10:19:10 -0400

    Create extraccion.py

---
+++ extraccion.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv('https://raw.githubusercontent.com/pedroest13/PR2/main/Housing.csv')
+data.head()
